d1b753066845dc89c8881dccda178aedb939d1eb
Stephan Chernichenko <stephan.savchenko@gmail.com>
2020-12-11 18:15:13 +0300

    security commit

--- src/main/java/com/titles/downloadservice/Controller/NYTAPIFetcher.java
+++ src/main/java/com/titles/downloadservice/Controller/NYTAPIFetcher.java
@@ -1,6 +1,5 @@
 package com.titles.downloadservice.Controller;
 
 // https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=key
-// api-key=vT5AK7ZlIM17z07vUA8x67wnxYnYSbHO
 public class NYTAPIFetcher {
 }
